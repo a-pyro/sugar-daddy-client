b6847386ac7e9ae3d26cfe8e5c376c6a4ed495aa
a-pyro <ardi.germenji@gmail.com>
2023-01-11 11:35:33 +0100

    feat(app): test api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { useState, useEffect } from 'react'
+import { API_URL } from './env'
+
 function App() {
-  return <h1 className='text-3xl font-bold'>Hello world!</h1>
+  // Test call api
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    fetch(`${API_URL}/sweets`)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+  }, [])
+  return (
+    <>
+      <h1 className='text-3xl font-bold'>Hello world!</h1>
+      {JSON.stringify(data)}
+    </>
+  )
 }
 
 export default App
